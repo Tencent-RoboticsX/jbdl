10d9d54e1f6f2988567d464a6078f4a9bf9eccdf
mikechzhou <mikechzhou@tencent.com>
2021-02-21 20:12:44 +0800

    add TransformToPosition

--- test/jaxRBDL/Kinematics/TransformToPosition_test.py
+++ test/jaxRBDL/Kinematics/TransformToPosition_test.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from oct2py import octave
 import numpy as np
-from pyRBDL.Kinematics.TransformToPosition import TransformToPosition
+from jaxRBDL.Kinematics.TransformToPosition import TransformToPosition
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 OCTAVE_PATH = os.path.join(os.path.dirname(os.path.dirname(CURRENT_PATH)), "octave")
@@ -16,7 +16,7 @@ class TestTransformToPosition(unittest.TestCase):
         input = np.random.randn(*(6, 6))
         py_output = TransformToPosition(input)
         oct_output = octave.TransformToPosition(input)
-        self.assertAlmostEqual(np.sum(np.abs(py_output - oct_output)), 0.0, 14)
+        self.assertAlmostEqual(np.sum(np.abs(py_output - oct_output)), 0.0, 5)
 
 if __name__ == "__main__":
     unittest.main()
