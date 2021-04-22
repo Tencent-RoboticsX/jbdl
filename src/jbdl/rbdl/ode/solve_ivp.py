ec0cb62746540d56127763f35dbae7a5e1f5ad9c
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-04-22 19:57:12 +0800

    add ode event.

--- src/jbdl/rbdl/ode/solve_ivp.py
+++ src/jbdl/rbdl/ode/solve_ivp.py
@@ -4,7 +4,7 @@ from numpy.core.numeric import argwhere
 from scipy.integrate.quadpack import tplquad
 import jax.numpy as jnp
 
-def integrate_dynamics(func, y0, t_span, delta_t, event=None, args=None, rtol=1e-5, atol=1e-5, mxstep=jnp.inf):
+def integrate_dynamics(func, y0, t_span, de_righta_t, event=None, args=None, rtol=1e-5, atol=1e-5, mxstep=jnp.inf):
     t0, tf = t_span
 
     # lists to store the results
@@ -17,7 +17,7 @@ def integrate_dynamics(func, y0, t_span, delta_t, event=None, args=None, rtol=1e
 
     while t2 < tf: 
         t1 = t_eval[-1]
-        t2 = t1 + delta_t
+        t2 = t1 + de_righta_t
         if t2 > tf - atol:
             t2 = tf
         y1 = sol[-1]
@@ -26,73 +26,94 @@ def integrate_dynamics(func, y0, t_span, delta_t, event=None, args=None, rtol=1e
             y2 = odeint(func, y1, jnp.linspace(t1, t2, 2), *args)
         else:
             y2 = odeint(func, y1, jnp.linspace(t1, t2, 2))
-        t_eval += [t2]
-        sol += [y2[-1, :]]
-        
-        if event is not None:
+
+
+        if event is None:
+            t_eval += [t2]
+            sol += [y2[-1, :]]
+        else:
+            # t_eval += [t2]
+            # sol += [y2[-1, :]]
             # now evaluate the event at the last position
-            e += [event(sol[-1], t_eval[-1])]
+            next_e = event(y2[-1, :], t2)
+            # e += [next_e]
             # print(e)
 
-            if e[-2] > 0 and e[-1] < 0 :
+            if e[-1] > 0 and next_e < 0 :
                 # Event detected where the sign of the event has changed from positive to negative. The
-                # event is between xPt = X[-2] and xLt = X[-1]. run a modified bisect
+                # event is between t_left = X[-2] and t_right = X[-1]. run a modified bisect
                 # function to narrow down to find where event = 0
-                xLt = t_eval[-1]
-                fLt = sol[-1]
-                eLt = e[-1]
+                t_right = t2
+                y_right = y2[-1, :]
+                e_right = next_e
 
-                xPt = t_eval[-2]
-                fPt = sol[-2]
-                ePt = e[-2]
+                t_left = t_eval[-1]
+                y_left = sol[-1]
+                e_left = e[-1]
 
                 j = 0
                 while j < 100:
-                    if jnp.abs(xLt - xPt) < 1e-4:
+                    # print(j, t_left, t_right)
+                    if jnp.abs(e_right) < 1e-3:
                         # we know the interval to a prescribed precision now.
                         # print 'Event found between {0} and {1}'.format(x1t, x2t)
-                        print('t = {0}, event = {1}, y = {2}'.format(xLt, eLt, fLt))
-                        events += [(xLt, fLt)]
+                        print('t = {0}, event = {1}, y = {2}'.format(t_right, e_right, y_right))
+                        events += [(t_right, y_right)]
 
-                        # Deal with event.
-                        # t_eval[-1] = xLt
-                        # sol[-1] = -fLt
+                        t2 = t_right
+                        next_e = e_right
 
+                        # print(y1, t1, t2)
+
+                        if args is not None:      
+                            y2 = odeint(func, y1, jnp.linspace(t1, t2, 2), *args)
+                        else:
+                            y2 = odeint(func, y1, jnp.linspace(t1, t2, 2))
+
+                        y2 = -y2
                         break # and return to integrating
 
-                    m = (ePt - eLt)/(xPt - xLt) #slope of line connecting points
+                    m = (e_left - e_right)/(t_left - t_right) #slope of line connecting points
                                                 #bracketing zero
 
                     #estimated x where the zero is
-                    inc_xPt = - ePt / m
-                    const_inc_xPt = 0.618 * (xLt - xPt)
+                    inc_t_left = - e_left / m
+                    const_inc_t_left = 0.618 * (t_right - t_left)
 
-                    if inc_xPt > const_inc_xPt:     
-                        new_x = const_inc_xPt + xPt
+                    if inc_t_left > const_inc_t_left:     
+                        new_t = const_inc_t_left + t_left
                     else:
-                        new_x =  inc_xPt + xPt
+                        new_t =  inc_t_left + t_left
                     
 
                     # now get the new value of the integrated solution at that new x
                     if args is not None:
-                        f  = odeint(func, fPt, jnp.linspace(xPt, new_x, 2), *args)
+                        f  = odeint(func, y_left, jnp.linspace(t_left, new_t, 2), *args)
                     else:
-                        f  = odeint(func, fPt, jnp.linspace(xPt, new_x, 2))
+                        f  = odeint(func, y_left, jnp.linspace(t_left, new_t, 2))
                     
-                    new_f = f[-1, :]
-                    
-                    new_e = event(new_f, new_x)
+                    new_y = f[-1, :]
+                    new_e = event(new_y, new_t)
+
 
                     if new_e > 0:
-                        xPt = new_x
-                        fPt = new_f
-                        ePt = new_e
+                        t_left = new_t
+                        y_left = new_y
+                        e_left = new_e
                     else:
-                        xLt = new_x
-                        fLt = new_f
-                        eLt = new_e
+                        t_right = new_t
+                        y_right = new_y
+                        e_right = new_e
 
                     j += 1
+        
+                t_eval += [t2]
+                sol += [y2[-1, :]]
+                e += [next_e]
+            else:
+                t_eval += [t2]
+                sol += [y2[-1, :]]
+                e += [next_e]
     
     sol = jnp.vstack(sol)
 
@@ -114,28 +135,28 @@ if __name__ == "__main__":
     c = 5.0
     
     t_span = (0, 10)
-    delta_t = 0.1
+    de_righta_t = 0.1
     t_num = 102
 
     def event(y, t):
         return y[0] 
 
-    # event = None
+    event = None
 
-    # t_eval, sol = integrate_dynamics(pend, y0, t_span, delta_t, event=event, args=(b, c), rtol=1.4e-8, atol=1.4e-8, mxstep=jnp.inf)
+    # t_eval, sol = integrate_dynamics(pend, y0, t_span, de_righta_t, event=event, args=(b, c), rtol=1.4e-8, atol=1.4e-8, mxstep=jnp.inf)
 
     # print(t_eval)
     # print(len(t_eval))
     # print(sol.shape)
     # print(sol)
 
-    def forward(y0, t_span, delta_t, event, b, c):
-        t_eval, sol =  integrate_dynamics(pend, y0, t_span, delta_t, event, args=(b, c))
+    def forward(y0, t_span, de_righta_t, event, b, c):
+        t_eval, sol =  integrate_dynamics(pend, y0, t_span, de_righta_t, event, args=(b, c))
         yT = sol[-1, :]
         return yT
 
-    def forward_all(y0, t_span, delta_t, event, b, c):
-        t_eval, sol =  integrate_dynamics(pend, y0, t_span, delta_t, event, args=(b, c))
+    def forward_all(y0, t_span, de_righta_t, event, b, c):
+        t_eval, sol =  integrate_dynamics(pend, y0, t_span, de_righta_t, event, args=(b, c))
         return t_eval, sol
 
 
@@ -154,7 +175,8 @@ if __name__ == "__main__":
 
     # sol = dynamics_fwd_path(y0, t_span, t_num, b, c)
 
-    t_eval, sol = forward_all(y0, t_span, delta_t, event, b, c)
+    t_eval, sol = forward_all(y0, t_span, de_righta_t, event, b, c)
+    # print(t_eval)
     print(len(t_eval))
     print(sol.shape)
 
@@ -166,10 +188,10 @@ if __name__ == "__main__":
     # plt.grid()
     # plt.show()
 
-    # print(forward(y0, t_span, delta_t, event, b, c))
+    # print(forward(y0, t_span, de_righta_t, event, b, c))
 
 
-    print(jacrev(forward, argnums=[4, 5])(y0, t_span, delta_t, event, b, c))
+    print(jacrev(forward, argnums=[4, 5])(y0, t_span, de_righta_t, event, b, c))
 
 
     # print(jacrev(dynamics_fwd, argnums=[3, 4])(y0, tspan, tnum, b, c))
