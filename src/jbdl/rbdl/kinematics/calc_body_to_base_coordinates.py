50c6e8c452a8214e7a622599d059fdda76a20d9d
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-16 21:16:02 +0800

    clear calc_body_to base_coordinates.py

--- src/jbdl/rbdl/kinematics/calc_body_to_base_coordinates.py
+++ src/jbdl/rbdl/kinematics/calc_body_to_base_coordinates.py
@@ -1,27 +1,26 @@
-from typing import Pattern
+from functools import partial
 from jax.api import jit
 import numpy as np
 import jax.numpy as jnp
 from jbdl.rbdl.model import joint_model
 from jbdl.rbdl.math import x_trans
 from jbdl.rbdl.kinematics import transform_to_position
-from functools import partial
 from jbdl.rbdl.utils import xyz2int
 
 @partial(jit, static_argnums=(1, 2, 3, 4))
 def calc_body_to_base_coordinates_core(x_tree, parent, jtype, jaxis, body_id, q, point_pos):
-    X0 = []
+    x0 = []
     for i in range(body_id):
-        XJ, _ = joint_model(jtype[i], jaxis[i], q[i])
-        Xup = jnp.matmul(XJ, x_tree[i])
+        xj, _ = joint_model(jtype[i], jaxis[i], q[i])
+        x_up = jnp.matmul(xj, x_tree[i])
         if parent[i] == 0:
-            X0.append(Xup)
+            x0.append(x_up)
         else:
-            X0.append(jnp.matmul(Xup, X0[parent[i] - 1]))
-    
-    XT_point = x_trans(point_pos)
-    X0_point =  jnp.matmul(XT_point, X0[body_id - 1])
-    pos = transform_to_position(X0_point)
+            x0.append(jnp.matmul(x_up, x0[parent[i] - 1]))
+
+    x_final_point = x_trans(point_pos)
+    x0_point =  jnp.matmul(x_final_point, x0[body_id - 1])
+    pos = transform_to_position(x0_point)
     return pos
 
 
