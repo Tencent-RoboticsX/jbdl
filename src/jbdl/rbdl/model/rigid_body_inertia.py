7c79a14b85083dfad3c3391da5d33a5f77e14a86
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-17 23:36:11 +0800

    clear rigid_body_inertia xyz2int

--- src/jbdl/rbdl/model/rigid_body_inertia.py
+++ src/jbdl/rbdl/model/rigid_body_inertia.py
@@ -2,34 +2,36 @@ from jax.api import jit
 import jax.numpy as jnp
 
 @jit
-def init_Ic_by_cholesky(l):
+def init_ic_by_cholesky(l):
     """
     Args:
         l (jnp.Array): float(6,) non-zero entris of a upper triangle matrix: exp(lxx) lxy lxz exp(lyy) lyz exp(lzz)
 
     """
     flatten_l = jnp.reshape(l, (-1,))
-    L = jnp.array(
+    ll = jnp.array(
         [[jnp.exp(flatten_l[0]), flatten_l[1], flatten_l[2]],
          [0.0, jnp.exp(flatten_l[3]), flatten_l[4]],
          [0.0, 0.0, jnp.exp(flatten_l[5])]])
 
-    Ic = jnp.matmul(jnp.transpose(L),  L)
-    return Ic
+    ic = jnp.matmul(jnp.transpose(ll),  ll)
+    return ic
+
 
 @jit
-def rigid_body_inertia(m: float, c, I):
+def rigid_body_inertia(m: float, c, ic):
 
     flatten_c = jnp.reshape(c, (-1,))
-    C = jnp.array(
+    cc = jnp.array(
         [[0.0, -flatten_c[2], flatten_c[1]],
          [flatten_c[2], 0.0, -flatten_c[0]],
          [-flatten_c[1], flatten_c[0], 0.0]])
     rbi = jnp.vstack(
-        [jnp.hstack([I + m * jnp.matmul(C, jnp.transpose(C)), m * C]),
-         jnp.hstack([m * jnp.transpose(C), m * jnp.eye(3)])])
+        [jnp.hstack([ic + m * jnp.matmul(cc, jnp.transpose(cc)), m * cc]),
+         jnp.hstack([m * jnp.transpose(cc), m * jnp.eye(3)])])
 
     return rbi
+
 
 if __name__ == "__main__":
     from jax import random
@@ -37,6 +39,6 @@ if __name__ == "__main__":
     key = random.PRNGKey(0)
     m = 2.0
     c = jnp.array([0.0, 0.0, 0.0])
-    I = init_Ic_by_cholesky(jnp.array([1.0, 0.0, 0.0, 1.0, 0.0, 1.0]))
-    print(make_jaxpr(rigid_body_inertia)(m, c, I))
-    print(rigid_body_inertia(m, c, I))
+    ic = init_ic_by_cholesky(jnp.array([1.0, 0.0, 0.0, 1.0, 0.0, 1.0]))
+    print(make_jaxpr(rigid_body_inertia)(m, c, ic))
+    print(rigid_body_inertia(m, c, ic))
