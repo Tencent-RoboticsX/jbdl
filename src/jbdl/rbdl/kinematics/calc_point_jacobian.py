d8dc6b333e191f9b1d9e408e67eb3fcf34718bd6
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-17 23:09:18 +0800

    clear calc_point_jacobian.py

--- src/jbdl/rbdl/kinematics/calc_point_jacobian.py
+++ src/jbdl/rbdl/kinematics/calc_point_jacobian.py
@@ -1,43 +1,44 @@
+from functools import partial
 import numpy as np
 import jax.numpy as jnp
 from jbdl.rbdl.model import joint_model
 from jbdl.rbdl.math import x_trans, inverse_motion_space
 from jax.api import jit
-from functools import partial
 from jbdl.rbdl.utils import xyz2int
+
 
 @partial(jit, static_argnums=(1, 2, 3, 4, 5))
 def calc_point_jacobian_core(x_tree, parent, jtype, jaxis, nb, body_id, q, point_pos):
-    S = []
-    Xup = []
-    X0 = []
+    s = []
+    x_up = []
+    x0 = []
 
     for i in range(body_id):
-        XJ, Si = joint_model(jtype[i], jaxis[i], q[i])
-        S.append(Si)
-        Xup.append(jnp.matmul(XJ, x_tree[i]))
+        xj, si = joint_model(jtype[i], jaxis[i], q[i])
+        s.append(si)
+        x_up.append(jnp.matmul(xj, x_tree[i]))
         if parent[i] == 0:
-            X0.append(Xup[i])
+            x0.append(x_up[i])
         else:
-            X0.append(jnp.matmul(Xup[i], X0[parent[i]-1]))
+            x0.append(jnp.matmul(x_up[i], x0[parent[i]-1]))
 
-    XT_point = x_trans(point_pos)
-    X0_point = jnp.matmul(XT_point, X0[body_id-1])
+    x_final_point = x_trans(point_pos)
+    x0_point = jnp.matmul(x_final_point, x0[body_id-1])
 
-    j_p = body_id - 1
-    BJ = jnp.zeros((6, nb))
-    while j_p != -1:
-        Xe = jnp.matmul(X0_point, inverse_motion_space(X0[j_p]))
-        BJ = BJ.at[:, [j_p, ]].set(jnp.matmul(Xe, S[j_p]))
-        j_p = parent[j_p] - 1
+    jp = body_id - 1
+    bj = jnp.zeros((6, nb))
+    while jp != -1:
+        xe = jnp.matmul(x0_point, inverse_motion_space(x0[jp]))
+        bj = bj.at[:, [jp, ]].set(jnp.matmul(xe, s[jp]))
+        jp = parent[jp] - 1
 
-    E0 = jnp.transpose(X0_point[0:3, 0:3])
-    J = jnp.matmul(jnp.matmul(E0, jnp.hstack([ jnp.zeros((3, 3)), jnp.eye(3)])), BJ)
+    e0 = jnp.transpose(x0_point[0:3, 0:3])
+    jac = jnp.matmul(jnp.matmul(e0, jnp.hstack([ jnp.zeros((3, 3)), jnp.eye(3)])), bj)
 
-    return J
+    return jac
 
 
-def calc_point_jacobian(model: dict, q: np.ndarray, body_id: int, point_pos: np.ndarray)->np.ndarray:
+def calc_point_jacobian(model: dict, q: np.ndarray, body_id: int, point_pos: np.ndarray) -> np.ndarray:
     q = q.flatten()
     point_pos = point_pos.flatten()
     jtype = tuple(model['jtype'])
@@ -46,5 +47,5 @@ def calc_point_jacobian(model: dict, q: np.ndarray, body_id: int, point_pos: np.
     nb = model["nb"]
     x_tree = model['x_tree']
 
-    J = calc_point_jacobian_core(x_tree, tuple(parent), tuple(jtype), jaxis, nb, body_id, q, point_pos)
-    return J
+    jac = calc_point_jacobian_core(x_tree, tuple(parent), tuple(jtype), jaxis, nb, body_id, q, point_pos)
+    return jac
