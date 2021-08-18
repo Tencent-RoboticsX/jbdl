ed6b594ecad5a258360c546272c9def8cb477d65
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-18 15:01:59 +0800

    clear joint_model

--- src/jbdl/rbdl/model/joint_model.py
+++ src/jbdl/rbdl/model/joint_model.py
@@ -1,6 +1,6 @@
+from functools import partial
 from jbdl.rbdl.math import x_rotx, x_roty, x_rotz, x_trans
 import jax.numpy as jnp
-from functools import partial
 from jax.api import jit
 
 
@@ -11,33 +11,32 @@ def joint_model(jtype: int, jaxis: int, q: float):
         # revolute joint
         # print("revolute joint")
         if jaxis == 0:
-            Xj = x_rotx(q)
-            S = jnp.array([[1.0], [0.0], [0.0], [0.0], [0.0], [0.0]])
+            xj = x_rotx(q)
+            s = jnp.array([[1.0], [0.0], [0.0], [0.0], [0.0], [0.0]])
         if jaxis == 1:
-            Xj = x_roty(q)
-            S = jnp.array([[0.0], [1.0], [0.0], [0.0], [0.0], [0.0]])
+            xj = x_roty(q)
+            s = jnp.array([[0.0], [1.0], [0.0], [0.0], [0.0], [0.0]])
         if jaxis == 2:
-            Xj = x_rotz(q)
-            S = jnp.array([[0.0], [0.0], [1.0], [0.0], [0.0], [0.0]])
+            xj = x_rotz(q)
+            s = jnp.array([[0.0], [0.0], [1.0], [0.0], [0.0], [0.0]])
     if jtype == 1:
         # prismatic joint
         # print("prismatic joint")
         # print(jaxis)
         if jaxis == 0:
-            Xj = x_trans(jnp.array([[q], [0.0], [0.0]]))
-            S = jnp.array([[0.0], [0.0], [0.0], [1.0], [0.0], [0.0]])
+            xj = x_trans(jnp.array([[q], [0.0], [0.0]]))
+            s = jnp.array([[0.0], [0.0], [0.0], [1.0], [0.0], [0.0]])
         if jaxis == 1:
-            Xj = x_trans(jnp.array([[0.0], [q], [0.0]]))
-            S = jnp.array([[0.0], [0.0], [0.0], [0.0], [1.0], [0.0]])
+            xj = x_trans(jnp.array([[0.0], [q], [0.0]]))
+            s = jnp.array([[0.0], [0.0], [0.0], [0.0], [1.0], [0.0]])
         if jaxis == 2:
-            Xj = x_trans(jnp.array([[0.0], [0.0], [q]]))
-            S = jnp.array([[0.0], [0.0], [0.0], [0.0], [0.0], [1.0]])
-    
-    return (Xj, S)
+            xj = x_trans(jnp.array([[0.0], [0.0], [q]]))
+            s = jnp.array([[0.0], [0.0], [0.0], [0.0], [0.0], [1.0]])
+
+    return (xj, s)
 
 if __name__ == "__main__":
     from jax import make_jaxpr
     import math
-    import numpy as np
     print(make_jaxpr(joint_model, static_argnums=(0, 1))(0, 'y', math.pi))
     print(joint_model(0, 'y', math.pi))
