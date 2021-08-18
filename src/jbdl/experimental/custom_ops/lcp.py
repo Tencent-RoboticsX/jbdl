a39b6224e866eb77522fa77cd35c633bce1e125f
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-18 17:08:37 +0800

    clear lcp

--- src/jbdl/experimental/custom_ops/lcp.py
+++ src/jbdl/experimental/custom_ops/lcp.py
@@ -10,8 +10,8 @@ from jax.api import jacfwd, device_put
 from jax.lib import xla_bridge
 from jbdl.experimental import cpu_ops
 
-# Register the CPU XLA custom calls
 
+# Register the CPU XLA custom calls
 for name, value in cpu_ops.registrations().items():
     if "lcp" in name:
         xla_client.register_cpu_custom_call_target(name, value)
@@ -21,24 +21,12 @@ lcp_prim.multiple_results = True
 lcp_prim.def_impl(partial(xla.apply_primitive, lcp_prim))
 
 
-# This function exposes the primitive to user code and this is the only
-# public-facing function in this module
-# @trace("lcp")
 def lcp(hh, f, ll, k, lb, ub):
-    # We're going to apply array broadcasting here since the logic of our op
-    # is much simpler if we require the inputs to all have the same shapes
 
     x, z = lcp_prim.bind(hh, f, ll, k, lb, ub)
     return x, z
 
 
-# *********************************
-# *  SUPPORT FOR JIT COMPILATION  *
-# *********************************
-
-# For JIT compilation we need a function to evaluate the shape and dtype of the
-# outputs of our op for some given inputs
-# @trace("lcp_abstract_eval")
 def lcp_abstract_eval(hh, f, ll, k, lb, ub):
     nv = f.shape[0]
     nc = k.shape[0]
@@ -59,10 +47,6 @@ def lcp_abstract_eval(hh, f, ll, k, lb, ub):
     return (ShapedArray(primal_shape, dtype), ShapedArray(dual_shape, dtype))
 
 
-# We also need a translation rule to convert the function into an XLA op. In
-# our case this is the custom XLA op that we've written. We're wrapping two
-# translation rules into one here: one for the CPU and one for the GPU
-# @trace("lcp_translation")
 def cpu_lcp_translation(c, hh, f, ll, k, lb, ub):
     # The inputs have "shapes" that provide both the shape and the dtype
     hh_shape = c.get_shape(hh)
@@ -89,8 +73,6 @@ def cpu_lcp_translation(c, hh, f, ll, k, lb, ub):
     assert ub_shape.dimensions() == (nv, 1)
 
 
-
-
     # The input specification
     nv_array_shape = xla_client.Shape.array_shape(
         np.dtype(np.int64), (), ())
@@ -114,9 +96,6 @@ def cpu_lcp_translation(c, hh, f, ll, k, lb, ub):
         np.dtype(dtype), (nv, 1), (1, 0))
     dual_array_shape = xla_client.Shape.array_shape(
         np.dtype(dtype), (nc + 2 * nv, 1), (1, 0))
-
-    
-
 
 
     # We dispatch a different call depending on the dtype
@@ -148,10 +127,11 @@ def cpu_lcp_translation(c, hh, f, ll, k, lb, ub):
     )
 
 
-
 lcp_prim.def_abstract_eval(lcp_abstract_eval)
+
 # Connect the XLA translation rules for JIT compilation
 xla.backend_specific_translations["cpu"][lcp_prim] = cpu_lcp_translation
+
 
 def lcp_kkt(x, z,  hh, f, ll, k, lb, ub):
     n_var = hh.shape[1]
@@ -166,33 +146,22 @@ def lcp_kkt(x, z,  hh, f, ll, k, lb, ub):
     kkt = jnp.vstack([lagrange, inequality])
     return kkt
 
-# **********************************
-# *  SUPPORT FOR FORWARD AUTODIFF  *
-# **********************************
 
-# In this case we don't need to define a transpose rule in order to support
-# reverse and higher order differentiation. This might not be true in other
-# applications, so check out the "hhow JAX primitives work" tutorial in the JAX
-# documentation for more info as necessary.
 def lcp_jvp(arg_values, arg_tangents):
     hh, f, ll, k, lb, ub = arg_values
     hh_dot, f_dot, ll_dot, k_dot, lb_dot, ub_dot = arg_tangents
 
-    # We use "bind" here because we don't want to mod the mean anomaly again
     x_star, z_star = lcp_prim.bind(hh, f, ll, k, lb, ub)
     nv = hh.shape[1]
 
-
     dkkt2dx = jacfwd(lcp_kkt, argnums=0)(x_star, z_star,  hh, f, ll, k, lb, ub)
-    dkkt2dz = jacfwd(lcp_kkt, argnums=1)(x_star, z_star,  hh, f, ll, k, lb, ub)
-   
+    dkkt2dz = jacfwd(lcp_kkt, argnums=1)(x_star, z_star,  hh, f, ll, k, lb, ub)  
     dkkt2dhh = jacfwd(lcp_kkt, argnums=2)(x_star, z_star,  hh, f, ll, k, lb, ub)
     dkkt2df = jacfwd(lcp_kkt, argnums=3)(x_star, z_star,  hh, f, ll, k, lb, ub)
     dkkt2dll = jacfwd(lcp_kkt, argnums=4)(x_star, z_star,  hh, f, ll, k, lb, ub)
     dkkt2dk = jacfwd(lcp_kkt, argnums=5)(x_star, z_star,  hh, f, ll, k, lb, ub)
     dkkt2dlb = jacfwd(lcp_kkt, argnums=6)(x_star, z_star,  hh, f, ll, k, lb, ub)
     dkkt2dub = jacfwd(lcp_kkt, argnums=7)(x_star, z_star,  hh, f, ll, k, lb, ub)
-   
     dkkt2dxz = jnp.concatenate([dkkt2dx, dkkt2dz], axis=2)
     dkkt2dxz = jnp.transpose(dkkt2dxz, [3, 1, 0, 2])
     dkkt2dhh = jnp.transpose(dkkt2dhh, [2, 3, 0, 1])
@@ -201,32 +170,18 @@ def lcp_jvp(arg_values, arg_tangents):
     dkkt2dk = jnp.transpose(dkkt2dk, [2, 3, 0, 1])
     dkkt2dlb = jnp.transpose(dkkt2dlb, [2, 3, 0, 1])
     dkkt2dub = jnp.transpose(dkkt2dub, [2, 3, 0, 1])
-
     dxz2dhh = -jnp.linalg.solve(dkkt2dxz, dkkt2dhh)
     dxz2dhh = jnp.transpose(dxz2dhh, [2, 3, 0, 1])
-    # dx2dhh = dxz2dhh[0:nv, ...]
-    # dz2dhh = dxz2dhh[nv:, ...]
     dxz2df =  -jnp.linalg.solve(dkkt2dxz, dkkt2df)
     dxz2df = jnp.transpose(dxz2df, [2, 3, 0, 1])
-    # dx2df = dxz2df[0:nv, ...]
-    # dz2df = dxz2df[nv:, ...]
     dxz2dll = -jnp.linalg.solve(dkkt2dxz, dkkt2dll)
     dxz2dll = jnp.transpose(dxz2dll, [2, 3, 0, 1])
-    # dx2dll = dxz2dll[0:nv, ...]
-    # dz2dll = dxz2dll[nv:, ...]
     dxz2dk = -jnp.linalg.solve(dkkt2dxz, dkkt2dk)
     dxz2dk = jnp.transpose(dxz2dk, [2, 3, 0, 1])
-    # dx2dk = dxz2dk[0:nv, ...]
-    # dz2dk = dxz2dk[nv:, ...]
     dxz2dlb = -jnp.linalg.solve(dkkt2dxz, dkkt2dlb)
     dxz2dlb = jnp.transpose(dxz2dlb, [2, 3, 0, 1])
-    # dx2dlb = dxz2dlb[0:nv, ...]
-    # dz2dlb = dxz2df[nv:, ...]
     dxz2dub = -jnp.linalg.solve(dkkt2dxz, dkkt2dub)
     dxz2dub = jnp.transpose(dxz2dub, [2, 3, 0, 1])
-    # dx2dub = dxz2dub[0:nv, ...]
-    # dz2dub = dxz2dub[nv:, ...]
-
 
     if type(hh_dot) is  ad.Zero:
         diff_hh = device_put(0.0)
@@ -265,10 +220,6 @@ def lcp_jvp(arg_values, arg_tangents):
 ad.primitive_jvps[lcp_prim] = lcp_jvp
 
 
-# ************************************
-# *  SUPPORT FOR BATChhING WIThh VMAP  *
-# ************************************
-
 def lcp_batch(batched_args, batch_dims):
     hhs, fs, lls, ks, lbs, ubs = batched_args
     hhs_bd, fs_bd, lls_bd, ks_bd, lbs_bd, ubs_bd = batch_dims
@@ -292,11 +243,6 @@ def lcp_batch(batched_args, batch_dims):
 batching.primitive_batchers[lcp_prim] = lcp_batch
 
 
-    
-
-
-
-
 if __name__ == "__main__":
     from jax.config import config
     config.update("jax_enable_x64", True)
@@ -306,37 +252,12 @@ if __name__ == "__main__":
     k = np.array([2.0, 2.0, 3.0]).reshape(3, 1)
     lb = np.array([0.0, 0.0]).reshape(2, 1)
     ub = np.array([0.5, 5.0 ]).reshape(2, 1)
-    # print(hh.dtype)
-    # with expectNotImplementedError():
     print(jit(lcp)(hh, f, ll, k, lb, ub))
 
     fwd = jacfwd(lcp, argnums=2)(hh, f, ll, k, lb, ub)
     print(fwd)
-    # print(fwd.shape)
-
-    # from jax import random
-
-    # seed = 1701
-    # num_steps = 100
-    # key = random.PRNGKey(seed)
-    # sum = 0.0
-    # for i in range(num_steps):
-    #     key, subkey = random.split(key)
-    #     x = random.randint(subkey, (4, ), 0, 2)
-    #     print(x)
-    #     sum += lax.cond(
-    #         jnp.sum(x),
-    #         lambda _: jnp.ones((2, 1)),
-    #         lambda _: lcp(hh, f, ll, k, lb, ub),
-    #         operand = None
-    #     )
-    #     print(sum)
-
-    # print(sum)
 
     batch_size = 10
-
-    
 
     batch_hh = jnp.repeat(jnp.expand_dims(hh, axis=0), batch_size, axis=0)
     batch_f = jnp.repeat(jnp.expand_dims(f, axis=0), batch_size, axis=0)
@@ -356,9 +277,3 @@ if __name__ == "__main__":
 
     print(batch_z)
     print(batch_z.shape)
-    # print(fwd0)
-    # print(fwd0.shape)
-    # print(fwd1)
-    # print(fwd1.shape)
-
-    
