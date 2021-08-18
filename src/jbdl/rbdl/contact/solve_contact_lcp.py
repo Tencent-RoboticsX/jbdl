8870b62c686c6acb4e8a0747fafc322b53c2454c
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-18 20:02:11 +0800

    clear solve_contact_lcp.py

--- src/jbdl/rbdl/contact/solve_contact_lcp.py
+++ src/jbdl/rbdl/contact/solve_contact_lcp.py
@@ -16,12 +16,12 @@ else:
     from jbdl.experimental.custom_ops.lcp import lcp
 
 
-def get_A(mu, nf):
-    A = jnp.empty([])
+def get_aa(mu, nf):
+    aa = jnp.empty([])
     if nf == 2:  
         tx = jnp.array([1, 0])
         tz = jnp.array([0, 1])
-        A = jnp.vstack([-mu * tz + tx,
+        aa = jnp.vstack([-mu * tz + tx,
                         -mu * tz - tx,
                         -tz,
                         tz])
@@ -31,7 +31,7 @@ def get_A(mu, nf):
         ty = jnp.array([0, 1, 0])
         tz = jnp.array([0, 0, 1])
 
-        A = jnp.vstack([
+        aa = jnp.vstack([
             -mu * tz + tx,
             -mu * tz - tx,
             -mu * tz + ty,
@@ -39,7 +39,7 @@ def get_A(mu, nf):
             -tz,
             tz
         ])
-    return A
+    return aa
 
 def get_b(fzlb, fzub, nf):
     b = jnp.empty([])
@@ -49,12 +49,12 @@ def get_b(fzlb, fzub, nf):
         b = jnp.array([0.0, 0.0, 0.0, 0.0, -fzlb, fzub])
     return b
 
-def get_zero_A(nf):
-    A = jnp.empty([])
+def get_zero_aa(nf):
+    aa = jnp.empty([])
     if nf == 2:  
         tx = jnp.array([1.0, 0.0])
         tz = jnp.array([0.0, 1.0])
-        A = jnp.vstack([-tx,
+        aa = jnp.vstack([-tx,
                          tx,
                         -tz,
                         tz])
@@ -64,7 +64,7 @@ def get_zero_A(nf):
         ty = jnp.array([0.0, 1.0, 0.0])
         tz = jnp.array([0.0, 0.0, 1.0])
 
-        A = jnp.vstack([
+        aa = jnp.vstack([
             -tx,
             +tx,
             -ty,
@@ -72,7 +72,7 @@ def get_zero_A(nf):
             -tz,
             tz
         ])
-    return A
+    return aa
 
 def get_zero_b(nf):
     b = jnp.empty([])
@@ -82,22 +82,22 @@ def get_zero_b(nf):
         b = jnp.zeros((6,))
     return b
 
-def get_block_diag_A(flag_contact, mu, nf):
+def get_block_diag_aa(flag_contact, mu, nf):
     def f(carry, x):
         new_x = lax.cond(
             x,
-            lambda _: get_A(mu, nf),
-            lambda _: get_zero_A(nf),
+            lambda _: get_aa(mu, nf),
+            lambda _: get_zero_aa(nf),
             None
         )
         return carry, new_x
 
-    _, seq_A = lax.scan(f, None, flag_contact)
-    block_diag_A = jax.scipy.linalg.block_diag(*seq_A)
+    _, seq_aa = lax.scan(f, None, flag_contact)
+    block_diag_aa = jax.scipy.linalg.block_diag(*seq_aa)
 
-    return block_diag_A
+    return block_diag_aa
 
-def get_stack_b(flag_contact, fzlb, fzub,  nf):
+def get_stack_b(flag_contact, fzlb, fzub, nf):
     def f(carry, x):
         new_x = lax.cond(
             x,
@@ -109,44 +109,42 @@ def get_stack_b(flag_contact, fzlb, fzub,  nf):
 
     _, seq_b = lax.scan(f, None, flag_contact)
     stack_b = jnp.reshape(jnp.hstack(seq_b), (-1, 1))
-    # stack_b = jnp.reshape(stack_b, (-1, 1))
-    # block_diag_A = jax.scipy.linalg.block_diag(*seq_A)
 
     return stack_b
 
 
-def solve_contact_lcp_extend_core(x_tree, q, qdot, contactpoint, H, tau, C, contact_force_lb, contact_force_ub,\
+def solve_contact_lcp_extend_core(x_tree, q, qdot, contactpoint, hh, tau, cc, contact_force_lb, contact_force_ub,\
     idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf, ncp, mu):
 
-    Jc = calc_contact_jacobian_extend_core(x_tree, q, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
-    JcdotQdot = calc_contact_jdot_qdot_extend_core(x_tree, q, qdot, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
+    jc = calc_contact_jacobian_extend_core(x_tree, q, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
+    jcdot_qdot = calc_contact_jdot_qdot_extend_core(x_tree, q, qdot, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
     contact_force_lb = jnp.reshape(contact_force_lb, (-1,))
     contact_force_ub = jnp.reshape(contact_force_ub, (-1,))
     if nf == 2:
         contact_force_lb = jnp.array([contact_force_lb[0], contact_force_lb[2]])
         contact_force_ub = jnp.array([contact_force_ub[0], contact_force_ub[2]])
     tau = jnp.reshape(tau, (-1, 1))
-    C = jnp.reshape(C, (-1, 1))
-    M = jnp.matmul(Jc, jnp.linalg.solve(H, jnp.transpose(Jc)))
-    d0 = jnp.matmul(Jc, jnp.linalg.solve(H, tau - C))
-    d = jnp.add(d0, JcdotQdot)
+    cc = jnp.reshape(cc, (-1, 1))
+    mm = jnp.matmul(jc, jnp.linalg.solve(hh, jnp.transpose(jc)))
+    d0 = jnp.matmul(jc, jnp.linalg.solve(hh, tau - cc))
+    d = jnp.add(d0, jcdot_qdot)
 
     ncd = 2 + 2 * (nf -1)
 
-    A = jnp.zeros((ncd * nc, nf * nc))
+    aa = jnp.zeros((ncd * nc, nf * nc))
     b = jnp.zeros((ncd * nc, 1))
     lb = jnp.zeros((nf * nc, 1))
     ub = jnp.zeros((nf * nc, 1))
 
-    A = get_block_diag_A(flag_contact, mu, nf)
+    aa = get_block_diag_aa(flag_contact, mu, nf)
     b = get_stack_b(flag_contact, contact_force_lb[-1], contact_force_ub[-1], nf)
     lb = jnp.reshape(jnp.tile(contact_force_lb, nc), (-1, 1))
     ub = jnp.reshape(jnp.tile(contact_force_ub, nc), (-1, 1))
 
-    M = 0.5 * (M+jnp.transpose(M))
-    fqp, _ = lcp(M, d, A, b, lb, ub)
+    mm = 0.5 * (mm+jnp.transpose(mm))
+    fqp, _ = lcp(mm, d, aa, b, lb, ub)
 
-    flcp = jnp.matmul(jnp.transpose(Jc), fqp)
+    flcp = jnp.matmul(jnp.transpose(jc), fqp)
     flcp = jnp.reshape(flcp, (-1,))
 
     return flcp, fqp
@@ -154,39 +152,39 @@ def solve_contact_lcp_extend_core(x_tree, q, qdot, contactpoint, H, tau, C, cont
 
 
 
-def solve_contact_lcp_core(x_tree, q, qdot, contactpoint, H, tau, C, contact_force_lb, contact_force_ub,\
+def solve_contact_lcp_core(x_tree, q, qdot, contactpoint, hh, tau, cc, contact_force_lb, contact_force_ub,\
     idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf, ncp, mu):
 
-    Jc = calc_contact_jacobian_core(x_tree, q, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
-    JcdotQdot = calc_contact_jdot_qdot_core(x_tree, q, qdot, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
+    jc = calc_contact_jacobian_core(x_tree, q, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
+    jcdot_qdot = calc_contact_jdot_qdot_core(x_tree, q, qdot, contactpoint, idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf)
     contact_force_lb = jnp.reshape(contact_force_lb, (-1,))
     contact_force_ub = jnp.reshape(contact_force_ub, (-1,))
     if nf == 2:
         contact_force_lb = contact_force_lb[[0, 2]]
         contact_force_ub = contact_force_ub[[0, 2]]
     tau = jnp.reshape(tau, (-1, 1))
-    C = jnp.reshape(C, (-1, 1))
-    M = jnp.matmul(Jc, jnp.linalg.solve(H, jnp.transpose(Jc)))
-    d0 = jnp.matmul(Jc, jnp.linalg.solve(H, tau - C))
-    d = jnp.add(d0, JcdotQdot)
+    cc = jnp.reshape(cc, (-1, 1))
+    mm = jnp.matmul(jc, jnp.linalg.solve(hh, jnp.transpose(jc)))
+    d0 = jnp.matmul(jc, jnp.linalg.solve(hh, tau - cc))
+    d = jnp.add(d0, jcdot_qdot)
 
     ncd = 2 + 2 * (nf -1)
 
-    A = jnp.zeros((ncd * ncp, nf * ncp))
+    aa = jnp.zeros((ncd * ncp, nf * ncp))
     b = jnp.zeros((ncd * ncp, 1))
     lb = jnp.zeros((nf * ncp, 1))
     ub = jnp.zeros((nf * ncp, 1))
 
     for i in range(ncp):
-        A = A.at[i*ncd:(i+1)*ncd, i*nf:(i+1)*nf].set(get_A(mu, nf))
+        aa = aa.at[i*ncd:(i+1)*ncd, i*nf:(i+1)*nf].set(get_aa(mu, nf))
         b = b.at[i*ncd:(i+1)*ncd, 0].set(get_b(contact_force_lb[-1], contact_force_ub[-1], nf))
         lb = lb.at[i*nf:(i+1)*nf, 0].set(contact_force_lb)
         ub = ub.at[i*nf:(i+1)*nf, 0].set(contact_force_ub)
 
     # QP optimize contact force in world space
-    M = 0.5 * (M+jnp.transpose(M))
-    fqp = lcp(M, d, A, b, lb, ub)
-    flcp = jnp.matmul(jnp.transpose(Jc), fqp)
+    mm = 0.5 * (mm+jnp.transpose(mm))
+    fqp = lcp(mm, d, aa, b, lb, ub)
+    flcp = jnp.matmul(jnp.transpose(jc), fqp)
     flcp = jnp.reshape(flcp, (-1,))
 
     return flcp, fqp
@@ -208,8 +206,8 @@ def solve_contact_lcp(model: dict, q: np.ndarray, qdot: np.ndarray, tau: np.ndar
     jaxis = xyz2int(model["jaxis"])
     contactpoint = model["contactpoint"]
     flag_contact = flag_contact
-    H = model["H"]
-    C = model["C"]
+    hh = model["H"]
+    cc = model["C"]
     contact_cond = model["contact_cond"]
     contact_force_lb = contact_cond["contact_force_lb"].flatten()
     contact_force_ub = contact_cond["contact_force_ub"].flatten()
@@ -224,7 +222,7 @@ def solve_contact_lcp(model: dict, q: np.ndarray, qdot: np.ndarray, tau: np.ndar
             ncp = ncp + 1
 
 
-    flcp, fqp = solve_contact_lcp_core(x_tree, q, qdot, contactpoint, H, tau, C, contact_force_lb, contact_force_ub,\
+    flcp, fqp = solve_contact_lcp_core(x_tree, q, qdot, contactpoint, hh, tau, cc, contact_force_lb, contact_force_ub,\
         idcontact, flag_contact, parent, jtype, jaxis, nb, nc, nf, ncp, mu)
 
     fpd = np.zeros((3*nc, 1))
@@ -240,14 +238,14 @@ if __name__ == '__main__':
     flag_contact = jnp.array([1, 0, 0, 0])
 
     from jax import jacfwd, jacrev
-    print(get_A(mu, nf))
+    print(get_aa(mu, nf))
     print(get_b(fzlb, fzub, nf))
-    print(get_zero_A(nf))
+    print(get_zero_aa(nf))
     print(get_zero_b(nf))
 
-    print(get_block_diag_A(flag_contact, mu, nf))
+    print(get_block_diag_aa(flag_contact, mu, nf))
     print(get_stack_b(flag_contact, fzlb, fzub, nf))
-    # print(jacfwd(get_A, argnums=0)(mu, nf))
+    # print(jacfwd(get_aa, argnums=0)(mu, nf))
     # print(jacrev(get_b, argnums=0)(fzlb, fzub, nf))
     x = jnp.array([0.1, -0.2, 0.3])
     print(jnp.tile(x, 3))
