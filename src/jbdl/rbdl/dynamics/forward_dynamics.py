90a2f8a8c75ba4195557440a8e2fa8564d860346
wedthree <wx_08f71fc664674d5899780f8a0dbad524@git.code.tencent.com>
2021-08-18 14:31:24 +0800

    clear forward_dynamics.py v0

--- src/jbdl/rbdl/dynamics/forward_dynamics.py
+++ src/jbdl/rbdl/dynamics/forward_dynamics.py
@@ -8,60 +8,60 @@ from jbdl.rbdl.utils import xyz2int
 
 
 @partial(jit, static_argnums=(2, 3, 4, 5))
-def forward_dynamics_core(x_tree, I, parent, jtype, jaxis, nb, q, qdot, tau, a_grav):  
-    S = []
-    Xup = []
+def forward_dynamics_core(x_tree, inertia, parent, jtype, jaxis, nb, q, qdot, tau, a_grav):  
+    s = []
+    x_up = []
     v = []
     c = []
-    pA = []
-    IA = I.copy()
+    p_aa = []
+    inertia_aa = inertia.copy()
 
     for i in range(nb):
-        XJ, Si = joint_model(jtype[i], jaxis[i], q[i])
-        S.append(Si)
-        vJ = jnp.multiply(S[i], qdot[i])
-        Xup.append(jnp.matmul(XJ, x_tree[i]))
+        xj, si = joint_model(jtype[i], jaxis[i], q[i])
+        s.append(si)
+        vj = jnp.multiply(s[i], qdot[i])
+        x_up.append(jnp.matmul(xj, x_tree[i]))
         if parent[i] == 0:
-            v.append(vJ)
+            v.append(vj)
             c.append(jnp.zeros((6, 1)))
         else:
-            v.append(jnp.add(jnp.matmul(Xup[i], v[parent[i]-1]), vJ))
-            c.append(jnp.matmul(cross_motion_space(v[i]), vJ))
-        pA.append(jnp.matmul(cross_force_space(v[i]), jnp.matmul(IA[i], v[i])))
+            v.append(jnp.add(jnp.matmul(x_up[i], v[parent[i]-1]), vj))
+            c.append(jnp.matmul(cross_motion_space(v[i]), vj))
+        p_aa.append(jnp.matmul(cross_force_space(v[i]), jnp.matmul(inertia_aa[i], v[i])))
 
 
 
-    U = [jnp.empty((0,))] * nb
+    uu = [jnp.empty((0,))] * nb
     d = [jnp.empty((0,))] * nb
-    u = [jnp.empty((0,))] * nb 
+    u = [jnp.empty((0,))] * nb
 
     for i in range(nb-1, -1, -1):
-        U[i] = jnp.matmul(IA[i], S[i])
-        d[i] = jnp.squeeze(jnp.matmul(S[i].transpose(), U[i]))
-        u[i] = tau[i] - jnp.squeeze(jnp.matmul(S[i].transpose(), pA[i]))
+        uu[i] = jnp.matmul(inertia_aa[i], s[i])
+        d[i] = jnp.squeeze(jnp.matmul(s[i].transpose(), uu[i]))
+        u[i] = tau[i] - jnp.squeeze(jnp.matmul(s[i].transpose(), p_aa[i]))
         if parent[i] != 0:
-            Ia = IA[i] - jnp.matmul(U[i]/ d[i], jnp.transpose(U[i]))
-            pa = pA[i] + jnp.matmul(Ia, c[i]) + jnp.multiply(U[i], u[i]) / d[i]
-            IA[parent[i] - 1] = IA[parent[i] - 1] + jnp.matmul(jnp.matmul(jnp.transpose(Xup[i]), Ia), Xup[i])
-            pA[parent[i] - 1] = pA[parent[i] - 1] + jnp.matmul(jnp.transpose(Xup[i]), pa)
-
+            ia = inertia_aa[i] - jnp.matmul(uu[i] / d[i], jnp.transpose(uu[i]))
+            pa = p_aa[i] + jnp.matmul(ia, c[i]) + jnp.multiply(uu[i], u[i]) / d[i]
+            inertia_aa[parent[i] - 1] = inertia_aa[parent[i] - 1] + jnp.matmul(jnp.matmul(jnp.transpose(x_up[i]), ia), x_up[i])
+            p_aa[parent[i] - 1] = p_aa[parent[i] - 1] + jnp.matmul(jnp.transpose(x_up[i]), pa)
 
     a = []
     qddot = []
 
     for i in range(nb):
         if parent[i] == 0:
-            a.append(jnp.matmul(Xup[i],-a_grav) + c[i])
+            a.append(jnp.matmul(x_up[i], -a_grav) + c[i])
         else:
-            a.append(jnp.matmul(Xup[i], a[parent[i] - 1]) + c[i])
-        qddot.append((u[i] - jnp.squeeze(jnp.matmul(jnp.transpose(U[i]), a[i])))/d[i])
+            a.append(jnp.matmul(x_up[i], a[parent[i] - 1]) + c[i])
+        qddot.append((u[i] - jnp.squeeze(jnp.matmul(jnp.transpose(uu[i]), a[i])))/d[i])
         
-        a[i] = a[i] + jnp.multiply(S[i],  qddot[i])
+        a[i] = a[i] + jnp.multiply(s[i], qddot[i])
 
     qddot = jnp.reshape(jnp.stack(qddot), (nb, ))
     return qddot
 
-def forward_dynamics(model, q, qdot, tau):    
+
+def forward_dynamics(model, q, qdot, tau):
     q = q.flatten()
     qdot = qdot.flatten()
     tau = tau.flatten()
@@ -71,14 +71,7 @@ def forward_dynamics(model, q, qdot, tau):
     jaxis = xyz2int(model["jaxis"])
     parent = tuple(model["parent"])
     x_tree = model["x_tree"]
-    I = model["I"]
+    inertia = model["I"]
 
-    qddot = forward_dynamics_core(x_tree, I, tuple(parent), tuple(jtype), jaxis, nb, q, qdot, tau, a_grav)
+    qddot = forward_dynamics_core(x_tree, inertia, tuple(parent), tuple(jtype), jaxis, nb, q, qdot, tau, a_grav)
     return qddot
-
-
-
-
-
-            
-
