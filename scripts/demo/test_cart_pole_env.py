9f50f3b3b3945b6399c46dfbd63d21192afcb878
mikechzhou <mikechzhou@tencent.com>
2021-08-30 21:04:26 +0800

    fix cart_pole action input bugs

--- scripts/demo/test_cart_pole_env.py
+++ scripts/demo/test_cart_pole_env.py
@@ -2,13 +2,14 @@ from jbdl.envs.cart_pole_env import CartPole, DEFAULT_PURE_CART_POLE_PARAMS
 import jax.numpy as jnp
 import numpy as np
 
-env = CartPole(render=True)
-action = np.zeros((1,))
+env = CartPole(render=True, batch_size=2)
+action = np.zeros((2,1))
 print(env.state)
 for i in range(10000):
     next_state, reward, done, _ = env.step(action)
+    print(next_state)
     env.reset_render_state()
 
-    if done:
-        print("done")
-        env.reset(*DEFAULT_PURE_CART_POLE_PARAMS)
+    # if done:
+    #     print("done")
+    #     env.reset(*DEFAULT_PURE_CART_POLE_PARAMS)
