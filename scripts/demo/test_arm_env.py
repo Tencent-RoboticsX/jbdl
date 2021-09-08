039fa53cfe729559ea3d1d983ad73f95ca3c41bd
叶果 <michaelyeah7@126.com>
2021-09-07 22:53:07 -0500

    add test_arm

--- scripts/demo/test_arm_env.py
+++ scripts/demo/test_arm_env.py
@@ -8,11 +8,6 @@ state = jnp.array([q, qdot]).flatten()
 env.state = state
 action = jnp.zeros((7,))
 
-
-# x = jnp.array([1,2,3])
-# y = x[x>2]
-# print(y)
-
 for i in range(1000):
     print(i)
     next_state, reward, done, _ = env.step(action)
